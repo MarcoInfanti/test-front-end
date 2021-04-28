7e6703cd39c1ee228e3bb2bdef86c345aa6a8d48
marco infanti <marcoinfanti1994@gmail.com>
2021-04-28 12:32:50 -0400

    primer desafio

--- test-Frontend/src/app/get-list-teachers.service.ts
+++ test-Frontend/src/app/get-list-teachers.service.ts
@@ -2,23 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { characterReport } from './characterReport';
 
+
 @Injectable({
   providedIn: 'root'
 })
-export class GetListCharacterService {
+export class GetListTeachersService {
 
   constructor(private http:HttpClient) { }
   
-  url :string = "http://hp-api.herokuapp.com/api/characters/house/"
+  url :string = "http://hp-api.herokuapp.com/api/characters/staff"
 
   
 
-  public getCharacters(house){
+  public getTeachers(){
 
-    return this.http.get<characterReport[]>(this.url+house)
+    return this.http.get<characterReport[]>(this.url)
 
   }
 
+
 }
-
-
--- test-Frontend/src/app/lista-profesores/lista-profesores.component.ts
+++ test-Frontend/src/app/lista-profesores/lista-profesores.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSelectChange, MatSort,  MatTableDataSource } from '@angular/material';
+import {GetListTeachersService} from '../get-list-teachers.service';
+import {characterReport} from '../characterReport';
 
 @Component({
   selector: 'app-lista-profesores',
@@ -7,9 +10,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaProfesoresComponent implements OnInit {
 
-  constructor() { }
+  selectedValue ='';
+  age=0;
+  ELEMENT_DATA : any[] = [];
+  displayedColumns: string[] = ['name','patronus', 'dateOfBirth' , 'image'];
+  dataSourceTeacher = new MatTableDataSource<characterReport>(this.ELEMENT_DATA)
 
+
+  @ViewChild(MatSort , {static: false}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+
+  applyFilter(filterValue: string) {
+    this.dataSourceTeacher.filter = filterValue.trim().toLowerCase();
+    
+
+  }
+
+  getPatronus(patronus){
+
+    if (patronus == "") {
+
+    return "-"
+  } else {
+
+
+    return patronus
+  }
+
+  }
+
+  public calculateAge(date, year){
+
+     if (date) {
+
+
+      var newdate = date.split("-").reverse().join("-");
+      newdate = new Date(newdate)
+
+      console.log(newdate)
+      var timeDiff = Math.abs(Date.now() - newdate);
+    
+      this.age = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+      return this.age
+
+        } else if (year) {
+
+         var currentYear = new Date().getFullYear()
+         console.log(currentYear - year)
+         return currentYear - year
+
+
+        } else {
+
+          return "-"
+
+        }
+      
+
+  }
+
+  constructor(private service: GetListTeachersService) { }
+
+  public getAllTeachers(){
+    
+    
+    let response = this.service.getTeachers();
+    response.subscribe(report=>this.dataSourceTeacher.data=report as characterReport[])
+    console.log(this.dataSourceTeacher)
+  }
+
+ 
+  
   ngOnInit() {
+
+    this.getAllTeachers();
+
+  }
+
+  
+  ngAfterViewInit() {
+
+    this.dataSourceTeacher.paginator = this.paginator;
+    this.dataSourceTeacher.sort = this.sort;
+
   }
 
 }
