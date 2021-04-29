4ccec349c7adcb0f079233ec601c60174cb7dc56
marco infanti <marcoinfanti1994@gmail.com>
2021-04-28 22:17:54 -0400

    cargando imagen

--- test-Frontend/src/app/app.module.ts
+++ test-Frontend/src/app/app.module.ts
@@ -21,6 +21,9 @@ import { MatNativeDateModule } from '@angular/material';
 import { FormsModule  } from '@angular/forms';
 import { DatepipePipe } from './datepipe.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatRadioModule} from '@angular/material/radio';
+
 
 
 
@@ -54,6 +57,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     MatNativeDateModule,
     MatNativeDateModule,
     FormsModule,
+    MatCardModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- test-Frontend/src/app/student-request/student-request.component.ts
+++ test-Frontend/src/app/student-request/student-request.component.ts
@@ -8,32 +8,31 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class StudentRequestComponent implements OnInit {
 
-  constructor(private formBuilder:FormBuilder){
+  public imagePath;
+  imgURL: any;
+  public message: string;
 
-    
-  }
+  constructor(private formBuilder:FormBuilder){}
 
 
   StudentForm = this.formBuilder.group({
 
-    Name: ['', Validators.required, Validators.pattern('[a-zA-Z ]*')],
-    species: ['', Validators.required, Validators.pattern('[a-zA-Z ]*')],
-    gender: [''],
+    name: ['', Validators.required],
+    species: ['', Validators.required],
+    gender: ['', Validators.required],
     house: [''],
     dateOfBirth: ['',Validators.required],
     yearOfBirth:['', Validators.required],
-    ancestry: ['',Validators.required, Validators.pattern('[a-zA-Z ]*')],
-    eyeColour: ['', Validators.required, Validators.pattern('[a-zA-Z ]*')],
-    hairColour: ['',Validators.required, Validators.pattern('[a-zA-Z ]*')],
-    wood: ['',Validators.required, Validators.pattern('[a-zA-Z ]*')],
-    core: ['', Validators.required, Validators.pattern('[a-zA-Z ]*')],
+    ancestry: ['',Validators.required],
+    eyeColour: ['', Validators.required],
+    hairColour: ['',Validators.required],
+    wood: ['',Validators.required],
+    core: ['', Validators.required],
     length: [''],
-    patronus: ['', Validators.required, Validators.pattern('[a-zA-Z ]*')],
-    hogwartsStudent: [''],
-    hogwartsStaff: [''],
-    actor: ['', Validators.required, Validators.pattern('[a-zA-Z ]*')],
+    patronus: ['', Validators.required],
+    actor: ['', Validators.required],
     alive: [''],
-    image: ['', Validators.required, Validators.pattern('[a-zA-Z ]*')]
+    image: ['', Validators.required],
     
   })
 
@@ -42,6 +41,27 @@ export class StudentRequestComponent implements OnInit {
     console.log(' La informacion del FormBuilder es: ', this.StudentForm.value);
 
   }
+
+
+ //
+ public preview(files) {
+  if (files.length === 0)
+    return;
+  
+  var mimeType = files[0].type;
+  if (mimeType.match(/image\/*/) == null) {
+    this.message = "Only images are supported.";
+    return;
+  }
+
+  var reader = new FileReader();
+  this.imagePath = files;
+  reader.readAsDataURL(files[0]); 
+  reader.onload = (_event) => { 
+    this.imgURL = reader.result; 
+  }
+}
+  
 
   ngOnInit(){
 
