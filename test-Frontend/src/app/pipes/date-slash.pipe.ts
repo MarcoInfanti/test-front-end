ca23dad1475713085c5b4a053de55b7cfcb51bc4
marco infanti <marcoinfanti1994@gmail.com>
2021-04-30 17:21:00 -0400

    ver solicitudes terminado

---
+++ test-Frontend/src/app/pipes/date-slash.pipe.ts
@@ -0,0 +1,36 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateSlash'
+})
+export class DateSlashPipe implements PipeTransform {
+
+  age=0;
+
+  transform(date: any, year :any): any {
+
+    if (date) {
+
+
+      var newdate = date.split("/").reverse().join("/");
+      newdate = new Date(newdate)
+
+      var timeDiff = Math.abs(Date.now() - newdate);
+    
+      this.age = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+      return this.age
+
+      
+    } else if (year) {
+
+      var currentYear = new Date().getFullYear()
+      return currentYear - year
+
+
+  } else {
+
+    return "-"
+  }
+}
+
+}
